b821c96a21e2a4554e22466a8343ee30b04d42ff
unknown <manohar9.javadeveloper@gmail.com>
2022-09-26 21:58:09 +0530

    book dao is created

---
+++ src/main/java/com/ty/school/dao/ReferenceBookDAO.java
@@ -0,0 +1,60 @@
+package com.ty.school.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.ty.school.dto.ReferenceBook;
+
+
+public class ReferenceBookDAO {
+EntityManagerFactory entityManagerFactory=null;
+	
+	public ReferenceBook saveBook(ReferenceBook s)
+	{
+		entityManagerFactory=Persistence.createEntityManagerFactory("vikas");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(s);
+		entityTransaction.commit();
+		return s;
+		
+	}
+	public ReferenceBook findBook(int id)
+	{
+		entityManagerFactory=Persistence.createEntityManagerFactory("vikas");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		ReferenceBook s=entityManager.find(ReferenceBook.class, id);
+		return s;
+		
+	}
+	public boolean deleteBook(int id)
+	{
+		entityManagerFactory=Persistence.createEntityManagerFactory("vikas");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		ReferenceBook s=entityManager.find(ReferenceBook.class, id);
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		if (s!=null) {
+			entityTransaction.begin();
+			entityManager.remove(s);
+			entityTransaction.commit();
+			return true;
+		}
+		return false;
+		
+	}
+	public ReferenceBook updateStudent(ReferenceBook s)
+	{
+		entityManagerFactory=Persistence.createEntityManagerFactory("vikas");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		ReferenceBook s1=entityManager.merge(s);
+		entityTransaction.commit();
+		return s1;
+		
+	}
+
+}
